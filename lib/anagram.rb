fd04689e673f94b0fb7322419ae6837e457e803b
Jimmy <dm.a.uvarov@gmail.com>
2022-02-27 18:09:23 -0500

    commit tree 0ab9deaf7159587faa03a31574ceef79a277c579

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :matched_words
+    attr_reader :to_anagram_with
+
+    def initialize(to_anagram_with)
+        @to_anagram_with = to_anagram_with
+    end
+
+    def match(words)
+
+        a_to_anagram_with = @to_anagram_with.split("").sort
+        @matched_words = words.filter{ |word| word.split("").sort == a_to_anagram_with }
+        
+    end
+
+
+end
